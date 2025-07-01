7cfb0816c4eda1086c71ed2b394804216fa77036
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2025-07-01 23:24:59 +0530

    refactor: removed worklfow engine module

--- apps/api/src/workflow-registry/workflow-registry.module.ts
+++ apps/api/src/workflow-registry/workflow-registry.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WorkflowRegistryController } from './workflow-registry.controller';
 import { WorkflowRegistryService } from './workflow-registry.service';
-import { WorkflowEngineModule } from './services/workflow-engine.module';
 import { InternalWorkflowStorageModule } from '@internal-workflow/storage';
 
 @Module({
-  imports: [InternalWorkflowStorageModule, WorkflowEngineModule],
+  imports: [InternalWorkflowStorageModule],
   controllers: [WorkflowRegistryController],
   providers: [WorkflowRegistryService],
-  exports: [WorkflowRegistryService, WorkflowEngineModule],
+  exports: [WorkflowRegistryService],
 })
 export class WorkflowRegistryModule {}
