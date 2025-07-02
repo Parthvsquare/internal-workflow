42ae8bfdc7e1461d448b4f65e54c01abc8e624b1
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2025-07-02 21:05:22 +0530

    refactor: [wip] ts config shenanigans

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/web',
+  cacheDir: '../../node_modules/.vite/apps/frontend',
   server: {
     port: 3200,
     host: 'localhost',
